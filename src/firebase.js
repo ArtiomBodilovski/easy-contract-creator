36948ec5d93f1a640169819a81136b59ff25a82a
ArtiomBodilovski <112805603+ArtiomBodilovski@users.noreply.github.com>
2024-01-03 16:43:20 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import {getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDcnKNRWXYFhsO-d57DxdhUd9Tly5eC6FU",
-    authDomain: "easy-contract-creator.firebaseapp.com",
-    projectId: "easy-contract-creator",
-    storageBucket: "easy-contract-creator.appspot.com",
-    messagingSenderId: "1072370997558",
-    appId: "1:1072370997558:web:69d4200cc6a33a76a27b97",
-    measurementId: "G-ZDCL6ENX9M"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 // Initialize Firebase
